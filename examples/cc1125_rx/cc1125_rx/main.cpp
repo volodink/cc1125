742ef93bc499950325bda50a48d05b9263a04b93
Konstantin Volodin <volodin.konstantin@gmail.com>
2018-03-30 16:54:40 +0300

    Add files via upload

---
+++ examples/cc1125_rx/cc1125_rx/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * cc1125_rx.cpp
+ *
+ * Created: 30.03.2018 16:53:22
+ * Author : volodink
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+		asm("nop");
+    }
+}
+
