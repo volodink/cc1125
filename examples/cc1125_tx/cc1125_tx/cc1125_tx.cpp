5aa18a91b46dc3b67bb4f39bb0a533ebff354c50
Konstantin Volodin <volodin.konstantin@gmail.com>
2018-03-30 16:52:25 +0300

    Add files via upload

---
+++ examples/cc1125_tx/cc1125_tx/cc1125_tx.cpp
@@ -0,0 +1,19 @@
+/*
+ * cc1125_tx.cpp
+ *
+ * Created: 30.03.2018 16:48:30
+ * Author : volodink
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+
+    while (1) 
+    {
+		asm("nop");
+    }
+}
+
